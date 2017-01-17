25c31bd06efe36e2ef6bb3cc5e893ba0df10ed1a
Crackersmith <joshsmith117@yahoo.com>
2017-01-16 19:35:37 -0500

    Need Visual Studio
    Ran into an error with a script.

--- workers/unity/Assets/EntityTemplates/PlayerShipEntityTemplate.cs
+++ workers/unity/Assets/EntityTemplates/PlayerShipEntityTemplate.cs
@@ -1,33 +1,63 @@
 ﻿using UnityEngine;
+
 using Improbable.General;
+
 using Improbable.Math;
+
 using Improbable.Player;
+
 using Improbable.Ship;
+
 using Improbable.Unity.Core.Acls;
+
 using Improbable.Worker;
+
 
 namespace Assets.EntityTemplates
 {
+
     public class PlayerShipEntityTemplate : MonoBehaviour
+
     {
+
         // Template definition for a PlayerShip snapshot entity
+
         static public Entity GeneratePlayerShipEntityTemplate(string clientWorkerId, Coordinates initialPosition)
+
         {
+
             var playerEntityTemplate = new Entity();
+
             // Define components attached to entity
+
             playerEntityTemplate.Add(new WorldTransform.Data(new WorldTransformData(initialPosition, 0)));
+
             playerEntityTemplate.Add(new PlayerLifecycle.Data(new PlayerLifecycleData(0, 3, 10)));
+
             playerEntityTemplate.Add(new ShipControls.Data(new ShipControlsData(0, 0)));
+	    playerEntityTemplate.Add(new Health.Data(new HealthData(1000)));
+
 
             // Grant component access permissions
+
             var acl = Acl.Build()
+
                 .SetReadAccess(CommonPredicates.PhysicsOrVisual)
+
                 .SetWriteAccess<WorldTransform>(CommonPredicates.SpecificClientOnly(clientWorkerId))
+
                 .SetWriteAccess<ShipControls>(CommonPredicates.SpecificClientOnly(clientWorkerId))
+
                 .SetWriteAccess<PlayerLifecycle>(CommonPredicates.PhysicsOnly);
+
+		.SetWriteAccess<Health>(CommonPredicates.PhysicsOnly);
             playerEntityTemplate.SetAcl(acl);
 
+
             return playerEntityTemplate;
+
         }
+
     }
+
 }
--- workers/unity/Assets/Gamelogic/Pirates/Behaviours/DecrementHealth.cs
+++ workers/unity/Assets/Gamelogic/Pirates/Behaviours/DecrementHealth.cs
@@ -1,20 +1,46 @@
 ﻿using UnityEngine;
+
 using Improbable.Unity;
+
 using Improbable.Unity.Visualizer;
+
 
 
 namespace Assets.Gamelogic.Pirates.Behaviours
 {
+
     // Enable this MonoBehaviour on FSim (server-side) workers only
+
     [EngineType(EnginePlatform.FSim)]
+
     public class DecrementHealth : MonoBehaviour
+
     {
+
+    [Require]
+        private Health.Writer HealthWriter;
         void OnEnable()
+
         {
+
+		InvokeRepeating("DecrementCurrentHealth", 0, 1.0f);
         }
 
+
         void OnDisable()
+
         {
+
+		CancelInvoke("DecrementCurrentHealth");
+        }
+
+        void DecrementCurrentHealth()
+        {
+            if (HealthWriter.Data.currentHealth > 0)
+            {
+                HealthWriter.Send(new Health.Update().SetCurrentHealth(HealthWriter.Data.currentHealth - 20));
+            }
         }
     }
+
 }
